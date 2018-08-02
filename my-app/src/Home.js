222001f27d2fc433fe7b911c830916929f3853d7
vins-stha <shrs_samar@yahoo.com>
2018-08-02 14:33:47 +0300

    first react SPA project

---
+++ my-app/src/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component{
+	render() {
+		return (
+		  <div> <h2>hello</h2>
+			<p>Now that you've familiarized yourself with the basics of how to work with React, let's kick things up a few notches. What we are going to do is use React to build a simple single-page app (also referred to as SPA by the cool kids...and people living in Scandinavia). Like we talked about in our React introduction forever ago, single-page apps are different from the more traditiona
+		</p>
+
+		  
+                   </div>
+		);
+}
+}
+export default Home;
